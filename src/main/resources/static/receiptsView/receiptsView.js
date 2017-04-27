10b60a6880141dc85e6f0bf9da4c5bc1224fcf32
Poojabafnaprakash <poojabafnaprakash@gmail.com>
2017-04-26 22:03:02 -0700

    added editable table

--- src/main/resources/static/receiptsView/receiptsView.js
+++ src/main/resources/static/receiptsView/receiptsView.js
@@ -6,7 +6,7 @@ angular.module('app.receiptsView', ['ngRoute', 'ngStorage'])
     controller: 'ReceiptsViewCtrl'
   })
 }])
-.controller('ReceiptsViewCtrl', function($scope, $http,  $sessionStorage){
+.controller('ReceiptsViewCtrl', function($scope, $http){
 	$scope.itms = {};
 	$scope.itemsArray = [];
 		  $http({
@@ -16,4 +16,42 @@ angular.module('app.receiptsView', ['ngRoute', 'ngStorage'])
 					 $scope.itms = response.data;
 					 $scope.itemsArray = response.data.items;
 			});
+      // cancel all changes
+  $scope.cancel = function() {
+    for (var i = $scope.itemsArray.length; i--;) {
+      var item = $scope.itemsArray[i];
+      // undelete
+      if (item.isDeleted) {
+        delete item.isDeleted;
+      }
+      // remove new
+      if (item.isNew) {
+        $scope.itemsArray.splice(i, 1);
+      }
+    };
+  };
+
+  $scope.saveTable = function() {
+    var results = [];
+    for (var i = $scope.itemsArray.length; i--;) {
+      var item = $scope.itemsArray[i];
+      // actually delete user
+      if (item.isDeleted) {
+        $scope.itemsArray.splice(i, 1);
+      }
+      // mark as not new
+      if (item.isNew) {
+        item.isNew = false;
+      }
+      
+      console.log($scope.itemsArray);
+
+      // send on server
+      //results.push($http.post('/saveUser', user));
+    }
+
+    //return $q.all(results);
+  };
+
+
 });

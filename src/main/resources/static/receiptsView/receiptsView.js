379818b6277f30acc8fbcae9f59d6807b9fd6f77
Poojabafnaprakash <poojabafnaprakash@gmail.com>
2017-04-27 22:51:21 -0700

    modified receipts page

--- src/main/resources/static/receiptsView/receiptsView.js
+++ src/main/resources/static/receiptsView/receiptsView.js
@@ -16,6 +16,7 @@ angular.module('app.receiptsView', ['ngRoute', 'ngStorage'])
 					 $scope.itms = response.data;
 					 $scope.itemsArray = response.data.items;
 			});
+
       // cancel all changes
   $scope.cancel = function() {
     for (var i = $scope.itemsArray.length; i--;) {
@@ -44,13 +45,19 @@ angular.module('app.receiptsView', ['ngRoute', 'ngStorage'])
         item.isNew = false;
       }
       //results.push($http.post('/saveUser', user));
+      //console.log($scope.itms);
     }
-    
+    if($scope.split === 'I'){
+      $scope.itms.flag = 'I'
+    }
+    else{
+      $scope.itms.flag = 'T'
+    }
+
     // send on server
-    //console.log($scope.itms);
+    console.log($scope.itms);
 
     //return $q.all(results);
   };
-
 
 });
